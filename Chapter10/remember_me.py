d1c45a3ec28388d01ffb9b9da0c6e67a84338615
Renato Regalado <renato.regalado@gmail.com>
2020-02-29 20:23:32 -0600

    seperated into functions

--- Chapter10/remember_me.py
+++ Chapter10/remember_me.py
@@ -5,18 +5,31 @@ os.chdir(path)
 
 #Load the username, if it has been stored previously.
 #Otherwise, prompt for the username and store it
+def getStoredUsername():
+    filename = 'textFiles/username.json'
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
 
-filename = 'textFiles/username.json'
-
-try:
-    with open(filename) as f:
-        username = json.load(f)
-
-except FileNotFoundError:
+def getNewUsername():
+    #prompt for a new Username
+    filename = 'textFiles/username.json'
     username = input("What is your name? ")
     with open(filename, 'w') as f:
         json.dump(username, f)
+    return username
+
+def greetUser():
+    #greet user by name
+    username = getStoredUsername()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = getNewUsername()
         print(f"We'll remember you when you come back, {username}!")
 
-else:
-    print(f"Welcome back, {username}!")
+greetUser()
