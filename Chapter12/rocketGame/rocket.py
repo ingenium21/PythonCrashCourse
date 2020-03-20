c3dd9cfdd8450771809432c94e419fed22e790b8
Renato Regalado <renato.regalado@gmail.com>
2020-03-20 14:57:57 -0500

    added y axis

--- Chapter12/rocketGame/rocket.py
+++ Chapter12/rocketGame/rocket.py
@@ -25,28 +25,30 @@ class Rocket:
         self.rect = self.image.get_rect()
 
         #Start each new rocket at the bottom center of the screen
-        self.rect.center = self.screen_rect.center
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.centery = self.screen_rect.centery
+
 
         #Store a decimal value of the rocket's horizontal position
-        self.x = float(self.rect.x)
+        self.centerx = float(self.rect.x)
 
         #Store a decimal value of the rocket's vertical position
-        self.y = float(self.rect.y)
+        self.centery = float(self.rect.y)
     
     def update(self):
         """Update the rocket's position based on the movement flags"""
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.rocket_speed
+            self.centerx += self.settings.rocket_speed
         elif self.moving_left and self.rect.left > 0:
-            self.x -= self.settings.rocket_speed
-        elif self.moving_up and self.rect.up < self.screen_rect.up:
-            self.y += self.settings.rocket_speed
-        elif self.moving_down and self.rect.down > 0:
-            self.y -= self.settings.rocket_speed
+            self.centerx -= self.settings.rocket_speed
+        elif self.moving_up and self.rect.top > self.screen_rect.top:
+            self.centery -= self.settings.rocket_speed
+        elif self.moving_down and self.rect.bottom < self.screen_rect.bottom :
+            self.centery += self.settings.rocket_speed
         
         #update rect object from self.x and self.y
-        self.rect.x = self.x
-        self.rect.y = self.y
+        self.rect.centerx = self.centerx
+        self.rect.centery = self.centery
 
     def blitme(self):
         """Draw the rocket at it's current location"""
