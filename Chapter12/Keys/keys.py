f05dd22c342acce43cee91ecff2a27de53878222
Renato Regalado <renato.regalado@gmail.com>
2020-03-25 07:20:31 -0500

    added new exercise

--- Chapter12/Keys/keys.py
+++ Chapter12/Keys/keys.py
@@ -1,32 +1,30 @@
 """
 Author: Renato Regalado
 Created: March 16, 2020
-Exercise 12-2
-Find an image of a game character, place it in the center of the screen
-and match the background color of the image to the background of the screen.
+Exercise 12-5
+Keys: Make a Pygame file that creates an empty screen.  In the event loop, print the event.key attribute whenever a pygame.KEYDOWN event is detected
+Run the program and press various keys to see how Pygame responds
 """
 import os
 import sys
 import pygame
-from marius import Marius
 
 PATH = os.path.dirname(os.path.realpath(__file__))
 os.chdir(PATH)
 
-class BlueSky:
-    """Overall class to manage assets and behavior"""
+
+class Keys: 
+    """overrall class to manage assets and behavior"""
 
     def __init__(self):
-        """initialize the game and create resources"""
+        """Initialize the game and create resources"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Blue Sky")
+        self.screen = pygame.display.set_mode((800,600))
+        pygame.display.set_caption("Keys")
 
-        #set the background color
+        #Set the background color
         self.bg_color = (66, 158, 245)
-
-        self.marius = Marius(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -35,15 +33,16 @@ class BlueSky:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    print(event)
             
             #Redraw the screen during each pass through the loop
             self.screen.fill(self.bg_color)
-            self.marius.blitme()
 
-            #Make the most recently drawn screen visible
+            #MAke the most recently drawn screen visible
             pygame.display.flip()
 
 if __name__ == '__main__':
-    #Make a game instance and run it
-    bs = BlueSky()
-    bs.run_game()
+    #make a game instance and run it
+    k = Keys()
+    k.run_game()
